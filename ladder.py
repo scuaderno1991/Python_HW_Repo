5128ee26165686ced799f0491ca659695e2a1571
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-06 11:59:12 -0700

    pre

--- ladder.py
+++ ladder.py
@@ -10,6 +10,27 @@
     #return my_steps(s + 1)
 
 
+# def my_steps(n):
+#     if n <= 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     elif n == 2:
+#         return 2
+    
+#     # Create a list to store the number of ways to climb for each step.
+#     steps = [0] * (n + 1)
+    
+
+#     steps[1] = 1
+#     steps[2] = 2
+    
+   
+#     for i in range(3, n + 1):
+#         steps[i] = steps[i - 1] + steps[i - 2]
+    
+#     return steps[n]
+
 def my_steps(n):
     if n <= 0:
         return 0
@@ -17,19 +38,17 @@ def my_steps(n):
         return 1
     elif n == 2:
         return 2
-    
-    # Create a list to store the number of ways to climb for each step.
-    steps = [0] * (n + 1)
-    
 
-    steps[1] = 1
-    steps[2] = 2
-    
-    while 1 < n <= 25: 
-        for i in range(3, n + 1):
-            steps[i] = steps[i - 1] + steps[i - 2]
-    
-        return steps[n]
+    # Initialize variables to store the number of ways for the previous two steps.
+    pre = 2
+    pre1 = 1
+
+    for i in range(3, n + 1):
+        current = pre + pre1
+        pre1 = pre
+        pre = current
+
+    return pre
 
 #print(my_steps(2))
   
