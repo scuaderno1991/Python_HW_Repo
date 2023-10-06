951bbb3c3cb84edf6f32737c07edf7a38f9feeb0
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-06 12:19:53 -0700

    uploaded more files

---
+++ merge.py
@@ -0,0 +1,23 @@
+def merge_list(list1, list2):
+    sorted_list = []
+    sorted_list.extend(list1)
+    sorted_list.extend(list2)
+    
+    temp = 0
+    for i in range(0,len(sorted_list)):
+        min = i 
+        for j in range(0,len(sorted_list)):
+            if sorted_list[min] < sorted_list[j]:
+                temp = sorted_list[min]
+                sorted_list[min] = sorted_list[j]
+                sorted_list[j] = temp
+
+    print(f"Original List1: {list1} \nOriginal List2: {list2}")
+    
+    #sorted_list.sort(reverse = False)
+    print(f"Sorted List: {sorted_list}")
+    
+list1 = [10,78,99,4,8,2]
+list2 = [15,3,80,5,6]
+
+merge_list(list1,list2)
