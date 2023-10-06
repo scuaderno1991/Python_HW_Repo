29d0eb119eb78efc918cecb9877c2c23da84fb74
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-06 12:39:56 -0700

    saved

--- merge.py
+++ merge.py
@@ -3,18 +3,18 @@ def merge_list(list1, list2):
     sorted_list.extend(list1)
     sorted_list.extend(list2)
     
-    # if not isinstance(merge_list, int):
-    #     raise ValueError("input must be an integer")
-    
-    temp = 0
-    for i in range(0,len(sorted_list)):
-        min = i 
-        for j in range(0,len(sorted_list)):
-            if sorted_list[min] < sorted_list[j]:
-                temp = sorted_list[min]
-                sorted_list[min] = sorted_list[j]
-                sorted_list[j] = temp
-    return sorted_list   
+    if not isinstance(merge_list, int):
+        raise ValueError("input must be an integer")
+    else:
+        temp = 0
+        for i in range(0,len(sorted_list)):
+            min = i 
+            for j in range(0,len(sorted_list)):
+                if sorted_list[min] < sorted_list[j]:
+                    temp = sorted_list[min]
+                    sorted_list[min] = sorted_list[j]
+                    sorted_list[j] = temp
+        return sorted_list   
     #print(f"Original List1: {list1} \nOriginal List2: {list2}")
     
     #sorted_list.sort(reverse = False)
