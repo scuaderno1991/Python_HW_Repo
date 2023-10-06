22ffc55cdd4125741623b3c31460e0c7e2d2f77c
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-06 11:49:35 -0700

    changed code

--- ladder.py
+++ ladder.py
@@ -1,15 +1,35 @@
-def my_steps(n):
-    if n <= 1:
-        return n
-    while 1 < n < 25:
-        return my_steps(n-1) + my_steps(n-2)
+# def my_steps(n):
+#     if n <= 1:
+#         return n
+#         return my_steps(n-1) + my_steps(n-2)
 
 
-   
+# print(my_steps(2))
+ 
 #def countWays(s):
     #return my_steps(s + 1)
 
 
+def my_steps(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    
+    # Create a list to store the number of ways to climb for each step.
+    steps = [0] * (n + 1)
+    
 
+    steps[1] = 1
+    steps[2] = 2
+    
+ 
+    for i in range(3, n + 1):
+        steps[i] = steps[i - 1] + steps[i - 2]
+    
+    return steps[n]
 
+#print(my_steps(2))
   
