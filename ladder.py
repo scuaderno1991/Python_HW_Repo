ddc7c5b84d848d15abde5accc2b6bb8f66ff0a7a
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-06 11:28:42 -0700

    return fib

--- ladder.py
+++ ladder.py
@@ -1,7 +1,13 @@
-def my_steps(n):
+def fib(n):
     if n <= 1:
         return n
-    return my_steps(n-1) + my_steps(n-2)
+    return fib(n-1) + fib(n-2)
+
+def my_steps(n):
+    while 1 < n > 25 :
+        return fib(n+1)
+    
+
    
 #def countWays(s):
     #return my_steps(s + 1)
