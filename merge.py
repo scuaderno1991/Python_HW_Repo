598111ba5064db3d161f95570a0436cb3d3d6c82
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-06 17:02:34 -0700

    added TYPEERROR

--- merge.py
+++ merge.py
@@ -1,26 +1,28 @@
 def merge_list(list1, list2):
+    if not isinstance(list1, list) or not isinstance(list2, list):
+        raise TypeError("Both inputs must be lists.")
+    
     sorted_list = []
     sorted_list.extend(list1)
     sorted_list.extend(list2)
     
-    if not isinstance(merge_list, int):
-        raise TypeError("input must be an integer")
-    else:
-        temp = 0
-        for i in range(0,len(sorted_list)):
-            min = i 
-            for j in range(0,len(sorted_list)):
-                if sorted_list[min] < sorted_list[j]:
-                    temp = sorted_list[min]
-                    sorted_list[min] = sorted_list[j]
-                    sorted_list[j] = temp
-        return sorted_list   
+    temp = 0
+    for i in range(0,len(sorted_list)):
+        min = i 
+        for j in range(0,len(sorted_list)):
+            if sorted_list[min] < sorted_list[j]:
+                temp = sorted_list[min]
+                sorted_list[min] = sorted_list[j]
+                sorted_list[j] = temp
+    return sorted_list   
+    
+    
     #print(f"Original List1: {list1} \nOriginal List2: {list2}")
     
     #sorted_list.sort(reverse = False)
     #print(f"Sorted List: {sorted_list}")
-    
-#list1 = [10,78,99,4,8,2]
-#list2 = [15,3,80,5,6]
+######################################################################################
+# list1 = [10,78,99,4,8,2]
+# list2 = [15,3,80,5,6]
 
-#merge_list(list1,list2)
+# print(merge_list(list1,list2))
