3ed41e30ec7416e5c78d8773905a5a54b6861229
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-06 17:25:03 -0700

    Added Square

--- bug1.py
+++ bug1.py
@@ -3,6 +3,26 @@ class Base:
          self.x = x
          self.y = y
          self.size = size
+class Square(Base):
+        def __init__(self,x,y,size):
+            super().__init__(x,y,size)
+            
+        def shape(self):
+            print("This is a square")
+            
+        def draw(self):
+            return f""" ({self.x}, {self.y}, {self.size})
+        __________________________
+        |                        |
+        |                        |
+        |                        |
+        |                        |
+        |                        |
+        |                        |
+        |                        |
+        |________________________|
+        
+        """
 class Circle(Base):
     def __init__(self,x,size):
         super().__init__(x,x,size)
@@ -27,8 +47,12 @@ class Circle(Base):
         '  - , _ _ _ , '
 """
     
-def main():
-    s = Circle(2,1)
-    s.shape()
-    print(s.draw())
-main()
+# def main():
+#     s = Circle(2,1)
+#     s.shape()
+#     print(s.draw())
+    
+#     sq = Square(2,1,3)
+#     sq.shape()
+#     print(sq.draw())
+# main()
