639a44596ab56fd3189dc9023d83730359130f0e
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-08 17:48:46 -0700

    added

--- bug1.py
+++ bug1.py
@@ -8,8 +8,8 @@ class Square(Base):
             super().__init__(x,y)
             
         def shape(self):
-            print("This is a square")
-            
+            return f"This is a square"
+        
         def draw(self):
             return f""" ({self.x}, {self.y})
         __________________________
@@ -24,8 +24,8 @@ class Square(Base):
         
         """
 class Circle(Base):
-    def __init__(self,x):
-        super().__init__(x,x)
+    def __init__(self,x,y,size):
+        super().__init__(x,y,size)
         
     def shape(self):
         print("This is a circle")
@@ -33,6 +33,7 @@ class Circle(Base):
         
     def draw(self):
         return f""" ({self.x}, {self.y}) 
+
         
           , - ~ ~ ~ - ,
      , '                  ' ,
@@ -47,11 +48,12 @@ class Circle(Base):
         '  - , _ _ _ , '
 """
     
-# def main():
-#     s = Circle(2,1)
-#     s.shape()
-#     print(s.draw())
-    
+def main():
+    s = Circle(1,2,3)
+    print(s.shape())
+    print(s.draw())
+
+main()
 #     sq = Square(2,1,3)
 #     sq.shape()
 #     print(sq.draw())
