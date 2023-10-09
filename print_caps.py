d9523130cc3a83038842c76c94446e418aadc56c
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-08 20:37:52 -0700

    test

--- print_caps.py
+++ print_caps.py
@@ -1,14 +1,32 @@
 def allcaps(func):
-    def wrapper():
+    def wrapper(*args,**kwargs):
        
-        res = func()
-        if isinstance(res,str):
-            return res.capitalize()
+        res = func(*args, **kwargs)
+        if isinstance(res,list):
+            uppercase_list = [s.upper() for s in res]
+            return uppercase_list
         else:
             return res
 
     return wrapper
-   
+
+# @allcaps
+# def get_string():
+#     return ["apple", "oranges", "banana"]
+# result = get_string()
+
+# print(result)
+
+# def allcaps(func):
+#     def wrapper(*args,**kwargs):
+       
+#         res = func(*args, **kwargs)
+#         if isinstance(res,str):
+#             return res.capitalize()
+#         else:
+#             return res
+
+#     return wrapper 
   
 # @allcaps
 # def greet():
