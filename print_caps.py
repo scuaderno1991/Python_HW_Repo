3e6a293904c6833cdd9dc411269b7218678106ab
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-08 18:29:42 -0700

    added decorator

---
+++ print_caps.py
@@ -0,0 +1,20 @@
+def allcaps(func):
+    def wrapper():
+        res = func()
+        if isinstance(res,str):
+            return res.upper()
+        else:
+            return res
+
+    return wrapper
+   
+    
+# @allcaps
+# def greet():
+#     return "hello World!"
+
+# def main():
+#     greet()
+# main()
+
+# print(greet())
