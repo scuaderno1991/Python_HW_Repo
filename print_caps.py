01fed5ab0f69b4ea8bbf1c62c00d0c52d56eaa78
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-08 18:50:18 -0700

    test

--- print_caps.py
+++ print_caps.py
@@ -1,14 +1,15 @@
 def allcaps(func):
     def wrapper():
+       
         res = func()
         if isinstance(res,str):
-            return res.upper()
+            return res.capitalize()
         else:
             return res
 
     return wrapper
    
-    
+  
 # @allcaps
 # def greet():
 #     return "hello World!"
