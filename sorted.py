5e884e99a29d904f78299f84039467bec71595b3
scuaderno1991 <sammy.cuaderno@sjsu.edu>
2023-10-08 18:15:34 -0700

    added sortDictionary

--- sorted.py
+++ sorted.py
@@ -1,1 +1,21 @@
-def sort_dictionary(inp)
+def sort_dictionary(input_dict):
+    item = list(input_dict.items())
+    n = len(item)
+    
+    for i in range (1,len(item)):
+        current_dict = item[i]
+        j = i-1
+        
+        while j>= 0 and current_dict[0] > item[j][0]:
+            item[j+1] = item[j]
+            j -= 1
+            
+        item[j+1] = current_dict
+        
+    sort_tuples = [(name,values[0]) for name, values in item]
+    
+    return sort_tuples
+
+myDict = {'Tom': (5464512,24), 'Sara' : (5446987, 32), 'Mary': (1557896,20)}
+sorted_dict = sort_dictionary(myDict)
+print(sorted_dict)
