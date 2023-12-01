1b33e19b967f20c0d79df51796f2d74e657b6558
sammyboyking <sammy.cuaderno@sjsu.edu>
2023-12-01 10:10:04 -0800

    changed cati

--- cacti.py
+++ cacti.py
@@ -1,45 +1,42 @@
-def print_arr(arr):
-    for x in range(len(arr)):
-        for y in range(len(arr[0])):
-            print(arr[x][y], end=' ')
-        print()
-    print('==================================')
+
+
+######################################################################################################
+def cacti_number(func):
+    def wrapper(my_arr):
+        row = len(my_arr) # num of rows in matr
+        col = len(my_arr[0]) #num of columns
+        total = 0 
+        for j in range(row):
+            for k in range(col):
+                if my_arr[j][k] == 0:
+                    #  ele at the toprow or above is 0.
+                    #  ele at the bottom row or below  0.
+                    #  ele at the rightmost column or right is 0.
+                    #  ele at the top row or leftmost column or if the element at the top-left is zero.
+                    #  ele at the top row or rightmost column or if the element at the top-right is zero
+                    #  ele at the bottom row or leftmost column or if the element at the bottom-left is zero. 
+                     # ele at the bottom row or rightmost column or if the element at the bottom-right is zero   
+                    if (j == 0 or my_arr[j-1][k] == 0) and (j == row-1 or my_arr[j+1][k] == 0) and \
+                       (k == 0 or my_arr[j][k-1] == 0) and (k == col-1 or my_arr[j][k+1] == 0) and \
+                       (j == 0 or k == 0 or my_arr[j-1][k-1] == 0) and (j == 0 or k == col-1 or my_arr[j-1][k+1] == 0) and \
+                       (j == row-1 or k == 0 or my_arr[j+1][k-1] == 0) and(j == row-1 or k == col-1 or my_arr[j+1][k+1] == 0):
+                        my_arr[j][k] = 1 #if conditions are true set current element pos to 1
+                        total += 1 #sum number of 1s 
+        return total
+    return wrapper
+
+def main():
+    array = [
+        [1, 0, 1, 0, 1],
+        [0, 0, 0, 0, 0],
+        [1, 0, 0, 1, 1],
+        [1, 1, 1, 0, 1],
+        [1, 1, 0, 1, 0]
+    ]
+
     
-def cacti_number(plot):
-    def wrapper(arr): 
-        count = 0
-        rows = len(arr)
-        cols = len(arr[0])
-        # Check if the 2D array is rectangular
-        if any(len(row) != cols for row in arr):
-            return 0
-        FIRST_ROW = 0
-        FIRST_COL = 0
-        LAST_ROW = rows - 1
-        LAST_COL = cols - 1
-        
-            
-        for i in range(rows):
-            for j in range(cols):
-                if arr[i][j] == 1:
-                    continue
-                else:
-                    has_neighbor = False  # Flag to check if there are neighboring 1s
-                    if i > FIRST_ROW and arr[i-1][j] == 1:
-                        has_neighbor = True
-                    if j > FIRST_COL and arr[i][j-1] == 1:
-                        has_neighbor = True
-                    if i < LAST_ROW and arr[i+1][j] == 1:
-                        has_neighbor = True
-                    if j < LAST_COL and arr[i][j+1] == 1:
-                        has_neighbor = True
-                    
-                    if not has_neighbor:
-                        count += 1
-                        arr[i][j] = 1
-                
+    cacti_count = cacti_number(array)
 
-        return count
-
-    return wrapper(plot)
-
+    # Display the result.
+    print(f"Number of cacti is {cacti_count}")
+main()
